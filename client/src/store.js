cab35b0ec08edc17b586c0e1ca8c4ddff525da76
Mahjoub-Nasreddine <82827922+Mahjoub-Nasreddine@users.noreply.github.com>
2021-09-20 00:19:55 +0100

    init

---
+++ client/src/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, createStore, compose } from "redux";
+import thunk from "redux-thunk";
+
+import rootReducer from "./reducers";
+
+const initState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initState,
+  compose(
+    applyMiddleware(...middleware)
+  )
+);
+export default store;
