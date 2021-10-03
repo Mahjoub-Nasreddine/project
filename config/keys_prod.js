f1e385e9d6ff7f62bd10c326946d07d17dac8477
Mahjoub-Nasreddine <82827922+Mahjoub-Nasreddine@users.noreply.github.com>
2021-10-03 14:35:44 +0100

    Update keys_prod.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,9 @@
-module.exports = {
+/* module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY
+}; */
+
+module.exports = {
+    mongoURI: "mongodb+srv://nasreddine:0000@cluster0.uyd5k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    secretOrKey: "secret",
 };
